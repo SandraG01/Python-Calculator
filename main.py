b01ce5aa223932ac0b044bbef6098ec34404da52
Sandra Gurgzdaityte <sandragurgzdaityte@Sandras-MacBook-Air.local>
2022-02-28 17:27:18 +0000

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+
+number1 = 12
+
+first_name = "Sandra"
+last_name = "Gurgzdaityte"
+full_name = first_name +" "+ last_name
+print(full_name)
+age = 20
+
+print("Hello \nmy name is " + full_name + " and \nI am " + str(age))
